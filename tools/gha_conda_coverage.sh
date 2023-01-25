943f44458c3a7d6715e5b66e31dc7a8d3e3292d2
Francesco Biscani <bluescarni@gmail.com>
2023-01-25 08:49:32 +0100

    First attempt at coverage build.

---
+++ tools/gha_conda_coverage.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install wget
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/download/22.9.0-3/Mambaforge-22.9.0-3-Linux-x86_64.sh -O mambaforge.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/mambaforge/bin:$PATH"
+bash mambaforge.sh -b -p $HOME/mambaforge
+mamba env create -f cascade_devel.yml -q -p $deps_dir
+source activate $deps_dir
+
+export CXXFLAGS="$CXXFLAGS --coverage"
+
+mkdir build
+cd build
+
+cmake -G "Ninja" ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DCASCADE_BUILD_TESTS=yes -DBoost_NO_BOOST_CMAKE=ON
+
+cmake --build . -- -v
+
+ctest -j4 -VV
+
+# Upload coverage data.
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+chmod +x codecov
+./codecov -x $deps_dir/bin/gcov
+
+set +e
+set +x
