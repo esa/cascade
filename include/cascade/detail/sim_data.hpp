2fbcfa0d8c75e6429c967c9eb2e2b18c6c069665
Francesco Biscani <bluescarni@gmail.com>
2023-01-30 11:49:04 +0100

    Minor reorder for clarity.

--- include/cascade/detail/sim_data.hpp
+++ include/cascade/detail/sim_data.hpp
@@ -212,8 +212,6 @@ struct sim::sim_data {
     // Temporary buffer used in the construction of the BVH trees.
     std::vector<std::vector<std::uint32_t, detail::no_init_alloc<std::uint32_t>>> nc_buffer, ps_buffer, nplc_buffer;
 
-    // Chunk-local vectors of detected broad phase collisions between AABBs.
-    std::vector<oneapi::tbb::concurrent_vector<std::pair<size_type, size_type>>> bp_coll;
     // Data structure used during parallel broad phase collision detection.
     struct bp_data {
         // Local list of detected AABBs collisions.
@@ -225,6 +223,8 @@ struct sim::sim_data {
     // NOTE: indirect concurrent queue through a unique_ptr because otherwise
     // TBB puts a copy constructor requirement on the inner unique_ptr.
     std::vector<std::unique_ptr<oneapi::tbb::concurrent_queue<std::unique_ptr<bp_data>>>> bp_data_caches;
+    // Chunk-local vectors of detected broad phase collisions between AABBs.
+    std::vector<oneapi::tbb::concurrent_vector<std::pair<size_type, size_type>>> bp_coll;
 
     // Struct for holding polynomial caches used during
     // narrow phase collision detection.
