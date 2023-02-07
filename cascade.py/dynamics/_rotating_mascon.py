8994dd6665745dccd934ffe35f1a7b543da3f95d
Dario Izzo <dario.izzo@gmail.com>
2023-02-07 16:17:27 +0100

    towards the end of the cubesats notebook

--- cascade.py/dynamics/_rotating_mascon.py
+++ cascade.py/dynamics/_rotating_mascon.py
@@ -22,3 +22,17 @@ def rotating_mascon(Gconst, points, masses, omega):
     from heyoka import make_mascon_system
     return make_mascon_system(Gconst, points, masses, omega)
 
+def rotating_mascon_energy(state, Gconst, points, masses, omega):
+    import numpy as np
+    pe, qe, re = omega
+    kinetic = (state[3] * state[3] + state[4] * state[4] + state[5] * state[5]) / 2.
+    potential_g = 0.
+    for i in range(len(masses)):
+        distance = np.sqrt((state[0] - points[i][0]) * (state[0] - points[i][0])
+                         + (state[1] - points[i][1]) * (state[1] - points[i][1])
+                         + (state[2] - points[i][2]) * (state[2] - points[i][2]))
+        potential_g -= Gconst * masses[i] / distance
+    
+    potential_c = - 0.5 * (pe * pe + qe * qe + re * re) * (state[0] * state[0] + state[1] * state[1] + state[2] * state[2])  + 0.5 * (state[0] * pe + state[1] * qe + state[2] * re) * (state[0] * pe + state[1] * qe + state[2] * re)
+    return kinetic + potential_g + potential_c 
+
